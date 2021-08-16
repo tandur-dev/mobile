38f4e30312a89676b9621de9caa90c00346cab71
Rey <firnasreyhan@gmail.com>
2021-08-16 14:54:47 +0700

    up

--- app/src/main/java/com/android/tandur/MainActivity.java
+++ app/src/main/java/com/android/tandur/MainActivity.java
@@ -1,69 +1,18 @@
 package com.android.tandur;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.MenuItem;
 
-import com.android.tandur.bottom_navigation.favorite.FavoriteFragment;
-import com.android.tandur.bottom_navigation.home.HomeFragment;
-import com.android.tandur.bottom_navigation.profile.ProfileFragment;
-import com.android.tandur.bottom_navigation.transaction.TransactionFragment;
-import com.android.tandur.databinding.ActivityMainBinding;
 import com.android.tandur.view.urban_farming.SewakanLahanActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-//    private BottomNavigationView botnav;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        loadFragment(new HomeFragment());
-        //bottom navigation
-        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment;
-                switch (item.getItemId()){
-                    case R.id.navigation_home:
-                        fragment = new HomeFragment();
-                        loadFragment(fragment);
-                        return true;
-                    case R.id.navigation_favorite:
-                        fragment = new FavoriteFragment();
-                        loadFragment(fragment);
-                        return true;
-                    case R.id.navigation_transaction:
-                        fragment = new TransactionFragment();
-                        loadFragment(fragment);
-                        return true;
-                    case R.id.navigation_profile:
-                        fragment = new ProfileFragment();
-                        loadFragment(fragment);
-                        return true;
-                }
-                return false;
-            }
-        });
-
-    }
-
-    //load fragment
-    private void loadFragment(Fragment fragment){
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, SewakanLahanActivity.class));
     }
 }
